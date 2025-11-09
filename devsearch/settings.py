dbad211ecde4030d713c038bfea10319c1c3c38e
Soumendra Rout <soumendra096@gmail.com>
2025-11-09 12:18:13 +0530

    Updated finnal version of mail

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -196,8 +196,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'silur847@gmail.com'
-EMAIL_HOST_PASSWORD = 'ulibzrmpvfwwtfvj'
+EMAIL_HOST_USER = os.environ.get('GMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_APP_PASSWORD')
 
 
 # Static files (CSS, JavaScript, Images)
