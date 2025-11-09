8e971b922823dfc1a399ca3976960aa959f7041e
Soumendra Rout <soumendra096@gmail.com>
2025-11-09 13:14:06 +0530

    Updated finnal version of mail 2.4

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -201,9 +201,9 @@ CORS_ALLOW_ALL_ORIGINS=True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'apikey'  
 EMAIL_HOST_PASSWORD = os.environ.get('MY_SENDGRID_KEY ')
 DEFAULT_FROM_EMAIL =  os.environ.get('DEFAULT_FROM_EMAIL')
